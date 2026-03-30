ef046a03bc2ae03d090a340a06b7837fbf5bd905
CubeTheDev <cubethedev@outlook.it>
2026-03-30 22:37:32 +0200

    test Patreon API Response #2

--- auth.py
+++ auth.py
@@ -25,9 +25,23 @@ while True:
     #print("Trying to get player storage..")
     #send_command("data get storage jojo_recubed player")
     print("Testing Patreon API..")
-    url = "https://www.patreon.com/api/oauth2/v2/campaigns?include=memberships"
+    url = "https://www.patreon.com/api/oauth2/v2/members?include=currently_entitled_tiers&page[size]=100"
     headers = {"Authorization": f"Bearer {PATREON_TOKEN}"}
     resp = requests.get(url, headers=headers)
-    print(resp.status_code)
-    print(resp.json())
+    
+    print("Status code:", resp.status_code)
+    if resp.status_code == 200:
+        data = resp.json()
+        for member in data.get("data", []):
+            email = member["attributes"].get("email")
+            status = member["attributes"].get("patron_status")
+            amount = member["attributes"].get("currently_entitled_amount_cents")
+            print(f"Email: {email}, Status: {status}, Amount: {amount}")
+    else:
+        print(resp.json())
+    #url = "https://www.patreon.com/api/oauth2/v2/campaigns?include=memberships"
+    #headers = {"Authorization": f"Bearer {PATREON_TOKEN}"}
+    #resp = requests.get(url, headers=headers)
+    #print(resp.status_code)
+    #print(resp.json())
     time.sleep(30)
