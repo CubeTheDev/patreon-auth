97a06f937653172085cc2821c3ee5b2ccfeb5f2d
CubeTheDev <cubethedev@outlook.it>
2026-03-30 22:32:54 +0200

    test patreon API response

--- auth.py
+++ auth.py
@@ -1,10 +1,15 @@
 import time
+import json
+import requests
 from mcrcon import MCRcon
 
-# REPLACE THESE WITH YOUR DATA
+# RCON VARIABLES
 RCON_HOST = "srv1533907.hstgr.cloud"
 RCON_PASSWORD = "v3D3hT3buC_tfarc3n1M"
 RCON_PORT = 25575
+
+# PATREON VARIABLES
+PATREON_TOKEN = "_4OzZLqR37DvvoI41guxMPsomKZHniimpIn2h12GJeM"
 
 def send_command(cmd):
     try:
@@ -17,8 +22,12 @@ def send_command(cmd):
 
 # TEST LOOP
 while True:
-    print("Trying to get player storage..")
-    send_command("data get storage jojo_recubed player")
-    #/data get storage jojo_recubed player
-    #send_command("say Backend connected!")
+    #print("Trying to get player storage..")
+    #send_command("data get storage jojo_recubed player")
+    print("Testing Patreon API..")
+    url = "https://www.patreon.com/api/oauth2/v2/campaigns?include=memberships"
+    headers = {"Authorization": f"Bearer {PATREON_TOKEN}"}
+    resp = requests.get(url, headers=headers)
+    print(resp.status_code)
+    print(resp.json())
     time.sleep(30)
